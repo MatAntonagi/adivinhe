9c1e954bdfe955c5486b6fe4791d4587fb4db9d8
Matheus Toledo <mtoledoz650@gmail.com>
2025-12-04 21:19:55 -0300

    sorteando palavra, exibindo letras da palavra, listando letras uzadas, Criando a handleConfirm.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,91 @@
 import styles from "./app.module.css";
+import { useState, useEffect } from "react";
+import { WORDS, Challenge } from "./database/words";
 
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
-import { LettersUsed } from "./components/LettersUsed";
+import { LettersUsed, LettersUsedProps } from "./components/LettersUsed";
 
 export default function App() {
+  const [letter, setLetter] = useState("");
+  const [attempts, setAttempts] = useState(0);
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
 
   function handleRestartGame() {
     alert("Reiniciando o jogo...");
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+    setChallenge(randomWord);
+    setAttempts(0);
+    setLetter("");
+    setLettersUsed([]);
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return 
+    }
+
+    if (!letter.trim()) { 
+      return alert("Por favor, insira uma letra válida.");
+    }
+
+    const value = letter.toUpperCase();
+
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value);
+
+    if(exists) { 
+      alert(`Você já usou essa letra ${value}`)
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }])
+
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) {
+    return 
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
         
         <Tip tip="dica de Teste ;) !" />
 
         <div className={styles.word}>
-          <Letter value="T" />
-          <Letter value="E" />
-          <Letter value="S" />
-          <Letter value="T" />
-          <Letter value="E" />
-          <Letter value="" />
+          {challenge.word.split("").map(() => (
+            <Letter value=""/>
+            ))}
         </div>
 
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input maxLength={1} autoFocus placeholder="?" />
-          <Button title="Confirmar" />
+          <Input 
+          maxLength={1} 
+          autoFocus 
+          placeholder="?" 
+          value={letter}
+          onChange={e => setLetter(e.target.value)} />
+
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
 
         <div>
-          <LettersUsed />
+          <LettersUsed data={lettersUsed} />
         </div>
       </main>
     </div>
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,16 +1,29 @@
 import { Letter } from "../Letter"
 import style from "./style.module.css"
 
-function LettersUsed() {
+export type LettersUsedProps = {
+  value: string
+  correct: boolean
+}
+
+type Props = {
+  data: LettersUsedProps[]
+}
+
+function LettersUsed({ data }: Props) {
   return(
     <div className={style.lettersUsed}>
       <h5>Letras Ultilizadas</h5>
 
       <div>
-        <Letter value={"J"} size="small" color="wrong" />
-        <Letter value={"E"} size="small" color="correct"/>
-        <Letter value={"S"} size="small" color="correct"/>
-        <Letter value={"U"} size="small" color="wrong" />
+        {
+          data.map(({ value, correct }) => (
+            <Letter 
+            value={value} 
+            size="small" 
+            color={correct ? "correct" : "wrong"} />
+          ))
+        }
       </div>
     </div>
   )
